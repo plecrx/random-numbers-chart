f3e1e9cb2d1b8852751c0b053a8760d50a9c16a6
Prescilla Lecurieux <34810774+plecrx@users.noreply.github.com>
2024-01-13 16:20:44 +0100

    test(app): add missing tests (#3)
    * should properly call POST function
    
    * should load integers list before accessing home page
    
    * rm unrelevant file
    
    * should call search integers with correst params & set integers array
    
    * fix useRandomIntegerStore

--- src/utils/http/httpClient.ts
+++ src/utils/http/httpClient.ts
@@ -1,5 +1,5 @@
-export const httpClient = async (input: RequestInfo, init?: RequestInit): Promise<Response> => {
-    const response = await fetch(input, init)
+export const httpClient = async (url: string, options?: RequestInit): Promise<Response> => {
+    const response = await fetch(url, options)
     if (!response.ok) {
         throw new Error(`[${response.status}] ${response.statusText}`)
     }
--- tests/features/integersList/searchRandomIntegers.spec.ts
+++ tests/features/integersList/searchRandomIntegers.spec.ts
@@ -1,7 +1,34 @@
-import { describe, expect } from 'vitest'
+import { beforeEach, describe, expect, it, vitest } from 'vitest'
+import {
+    createSearchRandomIntegers,
+    SearchRandomIntegersDependencies,
+    SearchRandomIntegersParams, SearchRandomIntegersResponse
+} from '../../../src/features/integersList/searchRandomIntegers'
+import { Mocks } from '../../utils'
 
 describe('Search Random Integers Feature', () => {
-    it('should return true', () => {
-        expect(1 + 1).toBe(2)
+    let mocks: Mocks<SearchRandomIntegersDependencies>
+    let searchRandomIntegers: SearchRandomIntegersResponse
+    beforeEach(() => {
+        mocks = { httpPost: vitest.fn() }
+        searchRandomIntegers = createSearchRandomIntegers(mocks)
+    })
+
+    it('should properly call POST', async () => {
+        const params: SearchRandomIntegersParams = {
+            base: 2,
+            col: 1,
+            format: 'plain',
+            max: 10,
+            min: 0,
+            num: 200
+        }
+
+        await searchRandomIntegers(params)
+
+        expect(mocks.httpPost).toHaveBeenCalledWith(
+            `/integers/?num=200&min=0&max=10&col=1&base=2&format=plain`,
+            null
+        )
     })
 })
